af33579a6fdea724ed80ea502a04469288995d8f
Marco Angulo <marco@yaipan.com>
2021-08-14 12:32:53 -0600

    [Incidencia: SMC-370]
    Se documenta el web service de estado de cuenta proyectado.

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
     General: ['dominio', 'autenticacion', 'errores'],
-    'Web Services': ['integracion-conectividad', 'declaraciones', 'constancias', 'web-conectividad', 'reconexion-hidrometros', 'estado-cuenta', 'informacion-propiedades', 'apc-estados', 'recoleccion-ingresos', 'yaipay-persona', 'yaipay-cuenta'],
+    'Web Services': ['integracion-conectividad', 'declaraciones', 'constancias', 'web-conectividad', 'reconexion-hidrometros', 'estado-cuenta', 'estado-cuenta-proyectado', 'informacion-propiedades', 'apc-estados', 'recoleccion-ingresos', 'yaipay-persona', 'yaipay-cuenta'],
   },
 };
